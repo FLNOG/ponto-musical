e0363a137ea3a7479b466d77cd77ae400e5e6eaf
PedroHenriqueBianchini <ph879102@gmail.com>
2025-09-20 18:04:22 +0000

    criacao do carrinho/CarrinhoControler/carrinho.css

---
+++ src/main/java/com/ecommerce/loja/controller/CarrinhoController.java
@@ -0,0 +1,12 @@
+package com.ecommerce.loja.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CarrinhoController {
+
+    @GetMapping("/carrinho")
+    public String carrinho() {
+        return "Carrinho/carrinho";
+    }
+}
