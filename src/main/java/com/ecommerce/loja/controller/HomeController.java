6ceee49ba8281bbabf89f91bb1d8e2e0fb577743
Suyndara <souzamatheussilva41@gmail.com>
2025-09-22 19:37:05 -0300

    feat: Otimização do código para mapear a página "Sobre"

--- src/main/java/com/ecommerce/loja/controller/HomeController.java
+++ src/main/java/com/ecommerce/loja/controller/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController {
     public String home(){
         return "home";
     }
+
+    @GetMapping("/sobre")
+    public String sobre(){
+        return "includes/sobre";
+    }
 }
